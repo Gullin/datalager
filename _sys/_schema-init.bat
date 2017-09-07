27c7cfd9512b111c8a973a41d16ccf0baf135c6b
GULLIN CHRISTIAN - SB <CHRAN7@landskrona.se>
2017-09-07 22:30:07 +0200

    Funktion för schema initiering

--- _sys/_schema-init.bat
+++ _sys/_schema-init.bat
@@ -1,17 +1,37 @@
 @ECHO OFF
 @CALL _global-settings
 
+
 REM CP 437 (DOS)
 REM Argument 1: Processmodulsnamn
-REM Argument 2: Dataset vars schema-struktur ska l„sas av. Absoluta s”kv„gen anges vid filer. Format tolkas av FME.
+REM Argument 2: Dataset vars schema-struktur ska l„sas av. F”r filer anges s”kv„g och f”r databas anges det namn i FME som definierar anslutningen.
+REM             Absoluta s”kv„gen anges vid filer.
 REM             F”r specifik fil ange exempelvis c:\katalog\dataset.shp
 REM             F”r flera filer ange exempelvis c:\katalog\*.shp
+REM             Vid databas beh”ver databasanslutningen vara f”rdefinierad i fmw-filen, formatets l„sare beh”ver vara tillagd som
+REM             "Workspace Resource" f”r att komma †t dess specifika parametrar och kopplad till databasanslutningen f”r databasl„sare.
+REM Argument 3: Format, FME:s kortnamn f”r formatet. Fungerar som signal f”r hur FME ska tolka datasetet.
+REM             ESRISHAPE           F”r ESRI shape-filer
+REM             ORACLE_SPATIAL      F”r databas Oracle och datatypen spatial
+REM Argument 4: Tabeller, anv„nds n„r datasetet „r en databas f”r tabellnamn. Flera tabellnamn listas med mellanrum som separator och
+REM             inom situationstecken. Ej k„nslig f”r stora eller sm† bokst„ver.
+REM             Beh”ver ej v„rde om datasetet ej „r en databas. Argumentet beh”ver dock alltid komma sist f”r att kunna hantera ett icke-v„rde.
+
 
 IF NOT [%1]==[] (
     SET _arg1=%1
 )
 IF NOT [%2]==[] (
     SET _arg2=%2
+)
+IF NOT [%3]==[] (
+    SET _arg3=%3
+)
+REM Hanterar icke-v„rde f”r argumentet som definierar vilka tabeller som ska h„mtas vid databas som dataset
+IF NOT [%4]==[] (
+    SET _arg4=%4
+) ELSE (
+    SET _arg4=NULL
 )
 
 REM S„tts per bat-fil
@@ -57,6 +77,8 @@ REM Metoder
                         --ProcessName %DL_PROCESSID% ^
                         --RotDirectory %DL_ROTDIR% ^
                         --InData %_arg2% ^
+                        --InData-DatabasTable %_arg4% ^
+                        --DataFormat %_arg3% ^
                         --OutputDirectory %DL_ROTDIR%%_arg1%\_schema\
 
     IF %ERRORLEVEL% NEQ 0 (
