f195c99e6e294bd26c86a9ebfe46a5b9cee2d907
Christian Gullin <krich_80@hotmail.com>
2017-09-19 09:38:15 +0200

    Implementerat schema-validering och nödvändiga anpassningar för det.

--- _sys/_schema-driver.bat
+++ _sys/_schema-driver.bat
@@ -4,19 +4,20 @@
 
 REM CP 437 (DOS)
 REM Argument 1: Processmodulsnamn
-REM Argument 2: Suffix p† fil f”r schema-dokumentationen (standardsuffix "flush", f”r att inte skriva ”ver en ev. init-fil).
+REM Argument 2: Action f”r Workspace Drivern. Antar v„rde [write | validate]. ™vriga argument beh”ver ej ha v„rde vid "validate".
+REM Argument 3: Suffix p† fil f”r schema-dokumentationen (standardsuffix "flush", f”r att inte skriva ”ver en ev. init-fil).
 REM             Ska endast anta init eller flush. "init" ska endast anv„ndas n„r utg†ngsfil skapas f”r manifest. "flush" skapas vid varje datak”rning.
-REM             Validering mellan k”rningar g”rs med "init" eller "manifest" mot "flush".
-REM Argument 3: Dataset vars schema-struktur ska l„sas av. F”r filer anges s”kv„g och f”r databas anges det namn i FME som definierar anslutningen.
-REM             Absoluta s”kv„gen anges vid filer.
+REM             Validering mellan k”rningar g”rs med "manifest" mot "flush".
+REM Argument 4: Format, FME:s kortnamn f”r formatet. Fungerar som signal f”r hur FME ska tolka datasetet.
+REM             ESRISHAPE           F”r ESRI shape-filer
+REM             ORACLE_SPATIAL      F”r databas Oracle och datatypen spatial
+REM Argument 5: Dataset vars schema-struktur ska l„sas av.
+REM             F”r filer anges s”kv„g och f”r databas anges det namn i FME som definierar anslutningen. Absoluta s”kv„gen anges vid filer.
 REM             F”r specifik fil ange exempelvis c:\katalog\dataset.shp
 REM             F”r flera filer ange exempelvis c:\katalog\*.shp
 REM             Vid databas beh”ver databasanslutningen vara f”rdefinierad i fmw-filen, formatets l„sare beh”ver vara tillagd som
 REM             "Workspace Resource" f”r att komma †t dess specifika parametrar och kopplad till databasanslutningen f”r databasl„sare.
-REM Argument 4: Format, FME:s kortnamn f”r formatet. Fungerar som signal f”r hur FME ska tolka datasetet.
-REM             ESRISHAPE           F”r ESRI shape-filer
-REM             ORACLE_SPATIAL      F”r databas Oracle och datatypen spatial
-REM Argument 5: Tabeller, anv„nds n„r datasetet „r en databas f”r tabellnamn. Flera tabellnamn listas med mellanrum som separator och
+REM Argument 6: Tabeller, anv„nds n„r datasetet „r en databas f”r tabellnamn. Flera tabellnamn listas med mellanrum som separator och
 REM             inom situationstecken. Ej k„nslig f”r stora eller sm† bokst„ver.
 REM             Beh”ver ej v„rde om datasetet ej „r en databas. Argumentet beh”ver dock alltid komma sist f”r att kunna hantera ett icke-v„rde.
 
@@ -26,26 +27,31 @@ IF NOT [%1]==[] (
 )
 IF NOT [%2]==[] (
     SET _arg2=%2
-) ELSE (
-    SET _arg2=flush
 )
 IF NOT [%3]==[] (
     SET _arg3=%3
+) ELSE (
+    SET _arg3=flush
 )
 IF NOT [%4]==[] (
     SET _arg4=%4
 )
-REM Hanterar icke-v„rde f”r argumentet som definierar vilka tabeller som ska h„mtas vid databas som dataset
 IF NOT [%5]==[] (
     SET _arg5=%5
 ) ELSE (
     SET _arg5=NULL
 )
+REM Hanterar icke-v„rde f”r argumentet som definierar vilka tabeller som ska h„mtas vid databas som dataset
+IF NOT [%6]==[] (
+    SET _arg6=%6
+) ELSE (
+    SET _arg6=NULL
+)
 
 REM S„tts per bat-fil
 REM Namn f”r hel- eller delprocessen (modul) som batch-filen hanterar
 SET DL_PROCESSNAME=_schema
-SET DL_FMEPROCESS01="_sys\_schema.fmw"
+SET DL_FMEPROCESS01="_sys\_schema-driver.fmw"
 
 
 REM skapar tidsst„mpel och unikt process-ID f”r sp†rning av k”rd batch-process
@@ -84,10 +90,11 @@ REM Metoder
     @%DL_FMEFULLPATH% %DL_FMEPROCESS01% ^
                         --ProcessName %DL_PROCESSID% ^
                         --RotDirectory %DL_ROTDIR% ^
-                        --InData %_arg3% ^
-                        --InData-DatabasTable %_arg5% ^
+                        --DriverAction %_arg2% ^
+                        --InData %_arg5% ^
+                        --InData-DatabaseTable %_arg6% ^
                         --DataFormat %_arg4% ^
-                        --OutSchemaFileNameSuffix %_arg2%^
+                        --OutSchemaFileNameSuffix %_arg3%^
                         --OutputDirectory %DL_ROTDIR%%_arg1%\_schema\
 
     IF %ERRORLEVEL% NEQ 0 (
