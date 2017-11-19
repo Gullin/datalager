fd32e1a345869519f1dd2436e38c40ddab85e55a
Christian Gullin <krich_80@hotmail.com>
2017-11-19 13:23:57 +0100

    Ingen utskrift till console vid ändring av code page.

--- _sys/_log-batch.bat
+++ _sys/_log-batch.bat
@@ -8,6 +8,6 @@ SET DL_LOG_ARG2=%2
 :: skapar tidsst„mpel
 FOR /f "tokens=1,2" %%i IN ('_sys\_local-current-datetime standard') DO SET CurrentDateTime=%%i %%j
 
-CHCP 1252
+CHCP 1252 > nul
 @ECHO %CurrentDateTime% ^| %DL_LOG_ARG1% ^| %DL_LOG_ARG2% >> %DL_LOGDIR%%DL_LOGFILE%
-CHCP 437
+CHCP 437 > nul
