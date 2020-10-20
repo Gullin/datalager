bf995a74059c3ea873a6b64dfe45305940d1ce52
Christian Gullin <krich_80@hotmail.com>
2020-10-20 09:08:07 +0200

    Ny processmodul anpassat data från td_bal

--- datalager.bat
+++ datalager.bat
@@ -69,6 +69,7 @@ IF %ERRORLEVEL% EQU 0 (
         @CALL lkr-postgis-td_detaljplan
         @CALL lkr-postgis-td_drk
         @CALL lkr-postgis-td_bal
+        @CALL lkr-postgis-td_bal_anpassat
         @CALL lm-geodataplatsen-fastighetskartan
         @CALL lst
         @CALL nvv
