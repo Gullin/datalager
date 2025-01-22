0d66d28007a3bdf2cf94d377218532d69a61b6db
Christian Gullin <krich_80@hotmail.com>
2025-01-22 08:47:13 +0100

    Ny processmodul Sokigo Flex Gata

--- datalager.bat
+++ datalager.bat
@@ -347,6 +347,7 @@ EXIT
     @CALL lkr-postgis-flex_grund
     @CALL lkr-postgis-flex_grund_anpassat
     @CALL lkr-postgis-flex_park
+    @CALL lkr-postgis-flex_gata
 
     @REM "lm-geodataplatsen-fastighetskartan" ska plockas bort i en kommande version. Ers„tts med "GSD-Fastighetskartan topografi, vektor"
     @REM @CALL lm-geodataplatsen-fastighetskartan
